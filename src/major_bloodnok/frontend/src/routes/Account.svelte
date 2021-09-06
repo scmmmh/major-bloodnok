617fb06d9abca938ed6e0efca234360e148d28aa
Mark Hall <mark.hall@work.room3b.eu>
2021-09-06 20:24:26 +0100

    Styling update

--- src/major_bloodnok/frontend/src/routes/Account.svelte
+++ src/major_bloodnok/frontend/src/routes/Account.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <h1 class="sr-only">Account</h1>
-<div class="flex flex-row flex-1 overflow-auto p-4">
+<div class="flex flex-row flex-1 overflow-hidden p-4">
     <nav class="flex-0 w-48 bg-blue-200">
         <ul>
             <li><NavItem href="/account" class="px-3 py-2 {$location.pathname === '/account' ? 'bg-blue-800 text-white' : ''}">Transactions</NavItem></li>
@@ -20,7 +20,7 @@
             <li><NavItem href="/account/upload" class="px-3 py-2 {$location.pathname === '/account/upload' ? 'bg-blue-800 text-white' : ''}">Upload</NavItem></li>
         </ul>
     </nav>
-    <div class="flex-1 px-4">
+    <div class="flex-1 px-4 overflow-auto">
         <Route path="/"><AccountTransactions/></Route>
         <Route path="/upload"><AccountUpload/></Route>
     </div>
